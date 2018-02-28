adcf1fb5665129d98746a69e08ab7bab4972ca79
Nikita Chernyshov <xpom55@mail.ru>
2018-02-28 15:17:34 +0300

    plot this resultes

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -31,6 +31,24 @@ regressor.fit(X_train, y_train) # закидываем данные для тр�
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
+# print(y_pred)
+# print(regressor.predict(X_train))
+
+# Visualising the Trainig set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualising the Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Experience')
+plt.ylabel('Salary')
+plt.show()
 
 
 
